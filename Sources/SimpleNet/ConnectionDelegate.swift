797baef4e2ae55c2ce56e4368304ffde41a4de3d
Morgan Lieberthal <morgan@jmorgan.org>
2016-11-01 15:23:48 -0600

    Update ConnectionDelegate

--- Sources/SimpleNet/ConnectionDelegate.swift
+++ Sources/SimpleNet/ConnectionDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import struct Foundation.Data
+import struct Dispatch.DispatchData
 
 /// The `ConnectionDelegate` protocol describes an interface that should be
 /// implemented by the delegate for an instance of the `Connection` class.
@@ -15,30 +16,43 @@ public protocol ConnectionDelegate {
   ///
   /// This method is optional, and the provided default implementation is a no-op.
   ///
-  /// - parameters:
-  ///   - connection: The connection sending the message
-  ///   - error: An error object containing details of why the connection failed
+  /// - parameter connection: The connection sending the message.
+  /// - parameter error: An error object containing details of why the connection failed.
   func connection(_ connection: Connection, didFailWithError: Swift.Error)
 
   /// Sent as a connection loads data incrementally.
   ///
   /// This method is optional, and the provided default implementation is a no-op.
   ///
-  /// - parameters:
-  ///   - connection: The connection sending the message
-  ///   - data: The newly available data. The delegate should concatenate the contents
-  ///           of each `data` object delivered to build up the complete data for the 
-  ///           request.
+  /// - parameter connection: The connection sending the message.
+  /// - parameter data: The newly available data. The delegate should concatenate the contents
+  ///             of each `data` object delivered to build up the complete data for the
+  ///             request.
   func connection(_ connection: Connection, didReceive data: Data)
 }
 
 /// Default implementations
 extension ConnectionDelegate {
   /// Empty
-  func connection(_ connection: Connection, didFailWithError: Swift.Error) {
+  public func connection(_ connection: Connection, didFailWithError: Swift.Error) {
   }
 
   /// Empty
-  func connection(_ connection: Connection, didReceive data: Data) {
+  public func connection(_ connection: Connection, didReceive data: Data) {
+  }
+
+  /// Convenience method for receiving a `DispatchData` structure.
+  ///
+  /// This method simply converts the `DispatchData` struct to a `Data` struct,
+  /// and calls `connection(_:didReceive:)` with the resulting `Data` struct.
+  ///
+  /// - parameter connection: The connection sending the message.
+  /// - parameter data: The newly available `DispatchData` structure.
+  func connection(_ connection: Connection, didReceive data: DispatchData) {
+    let someData: Data = data.withUnsafeBytes(body: { (bytePointer: UnsafePointer<UInt8>) in
+      return Data(bytes: bytePointer, count: data.count)
+    })
+
+    self.connection(connection, didReceive: someData)
   }
 }
